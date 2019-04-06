80920e4ecb406f8eddb791220c3d20529917cc30
Dan Correa <dancorrea3@gmail.com>
2019-04-06 13:23:22 -0400

    added api ajax and bootstrap image div

--- public/javascript/scripts.js
+++ public/javascript/scripts.js
@@ -14,4 +14,11 @@ var img = $('<img id="dynamic">');
 img.attr('src', responseObject.imgurl);
 img.appendTo('#imagediv');
 
-
+var APIkey = "f0958fd1e2c9b9dee0e63dd6ca3dfaa0"
+var eBidQueryURL = "ebidapilink" + artist + APIkey;
+$.ajax({
+    url: eBidQueryURL,
+    method: "GET"
+}).then(function (response) {
+    console.log(response);
+});
