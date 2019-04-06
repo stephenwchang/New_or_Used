9c8097ad776aa6b186762a2ddc42154deb60228a
Dan Correa <dancorrea3@gmail.com>
2019-04-06 12:20:28 -0400

    Adding Image section to js

--- public/javascript/scripts.js
+++ public/javascript/scripts.js
@@ -1,3 +1,17 @@
-/* eslint-disable */
-console.warn('Project One JS Initialized');
-/* eslint-enable */
+var config = {
+    apiKey: "AIzaSyAqu9o7oWo8Zycr-T4lhoCn0tl_vhsnn2Q",
+    authDomain: "api-price-comparison.firebaseapp.com",
+    databaseURL: "https://api-price-comparison.firebaseio.com",
+    projectId: "api-price-comparison",
+    storageBucket: "",
+    messagingSenderId: "970000015156"
+  };
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+var img = $('<img id="dynamic">');
+img.attr('src', responseObject.imgurl);
+img.appendTo('#imagediv');
+
+
