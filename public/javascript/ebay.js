7270e36a5d7a2660ab92755f47568f39bb017fd8
Stephen Chang <stephenwchang@gmail.com>
2019-04-08 17:19:13 -0400

    basic basic ebay search on link click functionality

--- public/javascript/ebay.js
+++ public/javascript/ebay.js
@@ -7,7 +7,7 @@ $(function () {
 $(document).on("click", ".ebay-output", function(){
     $(function () {
         $('.ebay-output').popover()
-    })    
+    })
 })
 
 function truncate(string, x) {
@@ -30,7 +30,7 @@ function prettyU(string) {
 
 var searchTerm = ""
 function ebaySearch() {
-    var queryURL = "http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&sortOrder=PricePlusShippingLowest&buyerPostalCode=92128&SERVICE-VERSION=1.13.0&SECURITY-APPNAME=RyanChes-EbaySear-PRD-d13d69895-95fa1322&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&keywords=" + searchTerm   
+    var queryURL = "http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.13.0&SECURITY-APPNAME=RyanChes-EbaySear-PRD-d13d69895-95fa1322&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&keywords=" + searchTerm
 
     $.ajax({
       url: queryURL,
@@ -40,16 +40,16 @@ function ebaySearch() {
         console.log(response.findItemsByKeywordsResponse[0].searchResult[0].item[0].sellingStatus.currentPrice)
         console.log(response.findItemsByKeywordsResponse[0].searchResult[0].item[0].galleryURL)
         console.log(response.findItemsByKeywordsResponse[0].searchResult[0].item[0].title)
-                
+
         for (i=0; i<response.findItemsByKeywordsResponse[0].searchResult[0].item.length; i++) {
-            
+
             var newDiv = $("<div>")
             newDiv.html(response.findItemsByKeywordsResponse[0].searchResult[0].item[i].title)
             newDiv.attr("class", "col-8 ebay-output")
-            newDiv.attr("data-content", "<a> Price:  $"+ response.findItemsByKeywordsResponse[0].searchResult[0].item[i].sellingStatus.currentPrice + "</a> <br> <img height='140px' src="+ response.findItemsByKeywordsResponse[0].searchResult[0].item[i].galleryURL + ">")
+            newDiv.attr("data-content", "<a> Price:  $"+ response.findItemsByKeywordsResponse[0].searchResult[0].item[i].sellingStatus[0].currentPrice[0].__value__ + "</a> <br> <img height='140px' src="+ response.findItemsByKeywordsResponse[0].searchResult[0].item[i].galleryURL + ">")
             newDiv.attr("data-toggle", "popover")
-            newDiv.attr("data-placement", "right")
             newDiv.attr("data-trigger", "focus")
+            newDiv.attr("data-placement", "right")
             newDiv.attr("tabindex", 0)
             newDiv.attr("title", response.findItemsByKeywordsResponse[0].searchResult[0].item[i].title)
             newDiv.attr("data-html", "true")
--- public/javascript/scripts.js
+++ public/javascript/scripts.js
@@ -5,9 +5,33 @@ $(function () {
 })
 
 $(document).on("click", ".walmart-output", function(){
+
+  // on results link click, trigger ebay search of model number
+  var modelNumber;
+  var currentDataContent;
+  var currentItem = $(this)
+
+  modelNumber = currentItem.attr("data-model-number")
+  currentDataContent = currentItem.attr("data-content")
+
+  var queryURL = "http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.13.0&SECURITY-APPNAME=RyanChes-EbaySear-PRD-d13d69895-95fa1322&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&keywords=" + modelNumber
+
+
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+    dataType: 'JSONP',
+  }).then(function(response) {
+          currentItem.attr("data-content", currentDataContent + response.findItemsByKeywordsResponse[0].searchResult[0].item[0].title + "<br> <a> Ebay Current Bid:  $"+ response.findItemsByKeywordsResponse[0].searchResult[0].item[i].sellingStatus[0].currentPrice[0].__value__ + "</a> <br> <img height='140px' src="+ response.findItemsByKeywordsResponse[0].searchResult[0].item[i].galleryURL + ">")
+      })
+
+
+  console.log(currentDataContent);
+  console.log(modelNumber);
+
     $(function () {
         $('.walmart-output').popover()
-    })    
+    })
 })
 
 function truncate(string, x) {
@@ -30,7 +54,7 @@ function prettyU(string) {
 
 var searchTerm = ""
 function walmartSearch() {
-    var queryURL = "https://api.walmartlabs.com/v1/search?apiKey=d7hjdvye4sky5cdwmmmtf3bf&query=" + searchTerm   
+    var queryURL = "https://api.walmartlabs.com/v1/search?apiKey=d7hjdvye4sky5cdwmmmtf3bf&query=" + searchTerm
 
     $.ajax({
       url: queryURL,
@@ -39,7 +63,7 @@ function walmartSearch() {
     }).then(function(response) {
         console.log(response)
 
-        
+
         for (i=0; i<response.items.length; i++) {
             var trunD = prettyU(response.items[i].shortDescription)
             console.log("prettyU check:  " + trunD)
@@ -49,6 +73,7 @@ function walmartSearch() {
             newDiv.attr("data-content", "<a> Price:  $"+ response.items[i].salePrice + "</a> <br> <a>" + truncate(trunD, 180) + "</a> <br> <img height='250px' width='250'px src="+ response.items[i].largeImage + ">")
             newDiv.attr("data-toggle", "popover")
             newDiv.attr("data-placement", "right")
+            newDiv.attr("data-model-number", response.items[i].modelNumber)
             newDiv.attr("data-trigger", "focus")
             newDiv.attr("tabindex", 0)
             newDiv.attr("title", response.items[i].name)
