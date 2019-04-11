4aa84f4b659bfbf29597904151d0829c46a9612a
soggytoast111 <joncurto@gmail.com>
2019-04-11 08:58:17 -0400

    not working

---
+++ public/javascript/ebay-graph.js
@@ -0,0 +1,34 @@
+$.backstretch("../images/background.gif");
+
+var queryURL = ""
+
+function ebaySearch() {
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+    dataType: 'JSONP',
+  }).then(function(response) {
+    console.log(response)
+  })
+} 
+
+$("#searchBTN").click(function(event){
+    event.preventDefault();
+    queryURL = "http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findCompletedItems&SERVICE-VERSION=1.13.0&SECURITY-APPNAME=RyanChes-EbaySear-PRD-d13d69895-95fa1322&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&productID.type=UPC&productID.value=" + 
+    $("#input").val() + 
+    "&itemFilter(0).name=SoldItemsOnly&itemFilter(0).value=true&sortOrder=PricePlusShippingLowest"
+    console.log(queryURL)
+    ebaySearch()
+})
+
+$("#input").keypress(function(){
+  var keycode = (event.keyCode ? event.keyCode : event.which);
+  if(keycode == '13'){
+    event.preventDefault();
+    queryURL = "http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findCompletedItems&SERVICE-VERSION=1.13.0&SECURITY-APPNAME=RyanChes-EbaySear-PRD-d13d69895-95fa1322&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&productID.type=UPC&productID.value=" + 
+    $("#input").val() + 
+    "&itemFilter(0).name=SoldItemsOnly&itemFilter(0).value=true&sortOrder=PricePlusShippingLowest"
+    console.log(queryURL)
+    ebaySearch()
+  }
+})
